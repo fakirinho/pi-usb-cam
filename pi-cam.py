fbc69f2bae2ff1600a80eedb695b8a86c685a347
Kacper Stawinski <kacper.stawinski@delaval.com>
2017-01-19 23:17:27 +0100

    basic working version with FTP upload by default

--- pi-cam.py
+++ pi-cam.py
@@ -1,7 +1,33 @@
-import pygame
 import pygame.camera
-from pygame.locals import *
+import ftplib
+import datetime
+import time
+import argparse
+import os
+
+parser = argparse.ArgumentParser()
+parser.add_argument("hostname", help="FTP hostname")
+parser.add_argument("username", help="username")
+parser.add_argument("password", help="password")
+args = parser.parse_args()
 
 pygame.init()
 pygame.camera.init()
 
+cam = pygame.camera.Camera("/dev/video0", (640, 480))
+cam.start()
+cam.get_controls()
+image = cam.get_image()
+pygame.image.save(image, "image.jpg")
+
+session = ftplib.FTP(args.hostname, args.username, args.password)
+image_to_be_uploaded = open("image.jpg", 'r')
+timestamp = time.time()
+filename = datetime.datetime.fromtimestamp(timestamp).strftime('%Y-%m-%d_%H:%M:%S') + '.jpg'
+session.storbinary('STOR ' + filename, image_to_be_uploaded)
+
+image_to_be_uploaded.close()
+os.remove("image.jpg")
+session.close()
+cam.stop()
+pygame.quit()
